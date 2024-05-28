9fe77f1d4f4ac0e46c4375b7cb456a59c22029b3
Nathan Hinthorne <nathanrh856@gmail.com>
2024-05-28 13:06:27 -0700

    book interface

---
+++ src/core/model/book.model.ts
@@ -0,0 +1,24 @@
+export interface IRatings {
+    average: number;
+    count: number;
+    rating_1: number;
+    rating_2: number;
+    rating_3: number;
+    rating_4: number;
+    rating_5: number;
+}
+  
+export interface IUrlIcon {
+    large: string;
+    small: string;
+}
+  
+export interface IBook {
+    isbn13: number;
+    authors: string;
+    publication: number;
+    original_title: string;
+    title: string;
+    ratings: IRatings;
+    icons: IUrlIcon;
+}
